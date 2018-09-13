06a7c9cfc38acab29d484f683630d5acd32fab4a
gunturkh <guntur.kh@gmail.com>
2018-09-14 00:31:11 +0700

    Configure env

--- api/routes/tickets/model.js
+++ api/routes/tickets/model.js
@@ -4,7 +4,8 @@ const user = process.env.MONGO_USER
 const pass = process.env.MONGO_PASS
 const db = process.env.MONGO_DATABASE
 const host = process.env.MONGO_HOST
-mongoose.connect(`mongodb://${user}:${pass}@${host}/${db}`)
+const port = process.env.MONGO_PORT
+mongoose.connect(`mongodb://${user}:${pass}@${host}:${port}/${db}`)
 // Define your schema as normal.
 const ticketSchema = mongoose.Schema({
     name: { type: String, required: true, unique: false },
--- config/config.js
+++ config/config.js
@@ -15,18 +15,21 @@ require('dotenv-extended').load({
       user: process.env.DEVELOPMENT_MONGO_USER,
       pass: process.env.DEVELOPMENT_MONGO_PASS,
       db: process.env.DEVELOPMENT_MONGO_DATABASE,
-      host: process.env.DEVELOPMENT_MONGO_HOST
+      host: process.env.DEVELOPMENT_MONGO_HOST,
+      port: process.env.DEVELOPMENT_MONGO_PORT
     },
     test: {
       user: process.env.TEST_MONGO_USER,
       pass: process.env.TEST_MONGO_PASS,
       db: process.env.TEST_MONGO_DATABASE,
-      host: process.env.TEST_MONGO_HOST
+      host: process.env.TEST_MONGO_HOST,
+      port: process.env.TEST_MONGO_PORT
     },
     production: {
       user: process.env.PRODUCTION_MONGO_USER,
       pass: process.env.PRODUCTION_MONGO_PASS,
       db: process.env.PRODUCTION_MONGO_DATABASE,
-      host: process.env.PRODUCTION_MONGO_HOST
+      host: process.env.PRODUCTION_MONGO_HOST,
+      port: process.env.PRODUCTION_MONGO_PORT
     }
   }
